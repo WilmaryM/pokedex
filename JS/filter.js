8cdae2935f55ee1dd5906f7183bcd6f87d71105f
Wilmary Mecedes <wilmarymercedes1404@gmail.com>
2022-10-03 23:00:23 -0400

    en proceso

--- JS/filter.js
+++ JS/filter.js
@@ -1,45 +1,50 @@
-console.log('hello word');
 
+// asi funciona correctamente la api
 const options = {
      method: 'GET',
      Headers:{
 }
 };
-fetch('https://pokeapi.co/api/v2/type', options)
-    .then(res=>  res.json())
+fetch('https://pokeapi.co/api/v2/', options)
+    .then(res =>  {
+        return res.json();
+    })
     .then(res => console.log(res))
-// .then(data => console.log(data))
     .catch(error => console.log('Error'))
 
 
+// ejemplo:
 
+let normal = document.getElementById("normal")
     function consultaPokemon(id){
-        fetch(` https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(function (res) {
+        fetch(`https://pokeapi.co/api/v2/type/${id}`)
+    .then((res) => {
         res.json()
-        .then(function (pokemon) {
-            console.log(pokemon.name)
-        })
+            .then(function (pokemon) {
+                console.log(pokemon.name);
+            });
     });
     };
-    consultaPokemon(4)
+    consultaPokemon(8)
 
-let busqueda = document.getElementById("busqueda")
-let normal = document.getElementById("normal")
-let grass = document.getElementById("grass")
-let bug = document.getElementById("bug")
-let water = document.getElementById("water")
-let fire = document.getElementById("Fire")
-let fight = document.getElementById("Fighting")
-let ele = document.getElementById("Electric")
-let rock = document.getElementById("Rock")
-let poison = document.getElementById("poison")
-let ghost = document.getElementById("ghost")
-let pysc = document.getElementById("pyschic")
-let ground = document.getElementById("ground")
-let fai = document.getElementById("fairy")
-let dra = document.getElementById("dragon")
-let ice = document.getElementById("ice")
-let rest = document.getElementById("rest")
+
+
+// let busqueda = document.getElementById("busqueda")
+// let normal = document.getElementById("normal")
+// let grass = document.getElementById("grass")
+// let bug = document.getElementById("bug")
+// let water = document.getElementById("water")
+// let fire = document.getElementById("Fire")
+// let fight = document.getElementById("Fighting")
+// let ele = document.getElementById("Electric")
+// let rock = document.getElementById("Rock")
+// let poison = document.getElementById("poison")
+// let ghost = document.getElementById("ghost")
+// let pysc = document.getElementById("pyschic")
+// let ground = document.getElementById("ground")
+// let fai = document.getElementById("fairy")
+// let dra = document.getElementById("dragon")
+// let ice = document.getElementById("ice")
+// let rest = document.getElementById("rest")
 
     
