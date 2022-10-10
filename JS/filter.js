2684565d69a711f681f34e0d01873c5d8689869a
Wilmary Mecedes <wilmarymercedes1404@gmail.com>
2022-10-10 15:50:53 -0400

    cambio de nombre

--- JS/filter.js
+++ JS/filter.js
@@ -15,21 +15,21 @@ fetch('https://pokeapi.co/api/v2/', options)
 
 
 
-    // searh pokemon 
+    // prueva
     
 const form = document.querySelector('#formulario_busqueda')
  const input = document.querySelector("#name")
 
     function consultaPokemon(id, num){
-        fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then((res) => {
         res.json()
             .then(function (pokemon) {
-             crearPokemon(pokemon)
+             crearPokemon(pokemon, num)
             });
     });
     };
-    consultaPokemon(2)
+    consultaPokemon(4)
 
 
     function busarPokeones() {
@@ -42,24 +42,5 @@ const form = document.querySelector('#formulario_busqueda')
     function crearPokemon(pokemon, num) {
         let item = busqueda.querySelector(`name${num}`)
     }
-crearPokemon()
+crearPokemon(5)
 
-
-// let normal = document.getElementById("normal")
-// let grass = document.getElementById("grass")
-// let bug = document.getElementById("bug")
-// let water = document.getElementById("water")
-// let fire = document.getElementById("Fire")
-// let fight = document.getElementById("Fighting")
-// let ele = document.getElementById("Electric")
-// let rock = document.getElementById("Rock")
-// let poison = document.getElementById("poison")
-// let ghost = document.getElementById("ghost")
-// let pysc = document.getElementById("pyschic")
-// let ground = document.getElementById("ground")
-// let fai = document.getElementById("fairy")
-// let dra = document.getElementById("dragon")
-// let ice = document.getElementById("ice")
-// let rest = document.getElementById("rest")
-
-    
