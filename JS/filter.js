b8b20ef9d736bc07f5f3033d338a2ad35d667920
Wilmary Mecedes <wilmarymercedes1404@gmail.com>
2022-10-13 14:33:12 -0400

    en progreso pokemon

--- JS/filter.js
+++ JS/filter.js
@@ -1,16 +1,27 @@
 
     const filterPokemon = async() => {
     try{
-        const Response = await  fetch('https://pokeapi.co/api/v2/')
+        const Response = await  fetch('https://pokeapi.co/api/v2/pokemon')
             console.log(Response);
         // si es correcta
         if(Response.status === 200){
          const data = await  Response.json();
-        console.log(data.)
+
+         let pokemon = '';
+        data.results.forEach(pokemon => {
+            console.log(pokemon.name);
+        });
+
+
+
+
         }else if(Response.status === 401){
             console.log('no ha sido ejecutada porque carece de credenciales válidas de autenticación')
         }else if(Response.status === 404){
-            console.log('No esiste ese dato')
+            console.log('No existe ese dato')
+        }
+        else{
+            console.log('Error no identificado')
         }
         
     }catch (error) {
